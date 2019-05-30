48d54f80f74a24d75d1a5fcf9ee387b3821ee211
Бальчугов Денис Дмитриевич <perfomeria@gmail.com>
2019-05-30 21:28:32 +0700

    feature(all): MunchkinGender "None" added

--- core-data/src/main/java/com/volkovmedia/component/data/model/MunchkinGender.kt
+++ core-data/src/main/java/com/volkovmedia/component/data/model/MunchkinGender.kt
@@ -1,6 +1,7 @@
 package com.volkovmedia.component.data.model
 
 enum class MunchkinGender {
+    NONE,
     MALE,
     FEMALE
 }
--- core-resource/src/main/java/com/volkovmedia/component/resource/MunchkinExt.kt
+++ core-resource/src/main/java/com/volkovmedia/component/resource/MunchkinExt.kt
@@ -4,29 +4,32 @@ import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.volkovmedia.component.data.model.MunchkinGender
-import com.volkovmedia.component.data.model.MunchkinGender.FEMALE
-import com.volkovmedia.component.data.model.MunchkinGender.MALE
+import com.volkovmedia.component.data.model.MunchkinGender.*
 
 val MunchkinGender.drawableResource: Int
     @DrawableRes get() = when (this) {
+        NONE -> R.drawable.ic_none
         MALE -> R.drawable.ic_male
         FEMALE -> R.drawable.ic_female
     }
 
 val MunchkinGender.colorResource: Int
     @ColorRes get() = when (this) {
+        NONE -> R.color.grey_medium
         MALE -> R.color.blue
         FEMALE -> R.color.pink
     }
 
 val MunchkinGender.colorLightResource: Int
     @ColorRes get() = when (this) {
+        NONE -> R.color.grey_light
         MALE -> R.color.blue_light
         FEMALE -> R.color.pink_light
     }
 
 val MunchkinGender.colorDarkResource: Int
     @ColorRes get() = when (this) {
+        NONE -> R.color.grey_dark
         MALE -> R.color.blue_dark
         FEMALE -> R.color.pink
     }
